1daa9e9a03473f1b3e5f93a1c235462f28df1af5
Juan <j.garrigos91@gmail.com>
2022-10-24 01:20:31 +0200

    pokeApp

--- cliente/pokeApp/app.js
+++ cliente/pokeApp/app.js
@@ -1,17 +1,58 @@
-import { axiosPokemonInfo } from './pokemon.mjs';
-
 const app = Vue.createApp({
     data() {
         return {
-            pokemon: '',
+            numPokemon: 0,
+            PokemonList: [],
             newPokemon: '',
+            pokemon: [],
+            moves: [],
+            urlImage: '',
+            exist: false,
+            listAll: false,
         }
     },
     methods:{
-        async findPokemon(){
-            const ditto = await axiosPokemonInfo(this.newPokemon);
-            console.log(ditto)
-            this.pokemon = ditto; 
+        async ListPokemon(){
+            try {
+                const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=${this.numPokemon}/`)
+                this.PokemonList = (response.data.results);
+            } catch (error) {
+              console.log(error)
+            }  
+        },
+        async showPokemon(){
+            try {
+                const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + this.newPokemon);
+                this.exist = true;
+                this.pokemon = response.data;
+                this.urlImage = response.data.sprites.front_default;
+                this.listLess();
+                console.log(this.pokemon.types);
+              } catch (error) {
+                console.log(error)
+              }
+        },
+        async listMore(){
+            this.listAll = true;
+            try{
+                const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + this.newPokemon);
+                this.moves = response.data.moves;
+            }catch (error) {
+                console.log(error)
+            }
+        },
+        async listLess(){
+            this.listAll = false;
+            try{
+                const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + this.newPokemon);
+                this.moves = response.data.moves.slice(0, 5);
+            }catch (error) {
+                console.log(error)
+            }
+        },
+        putPokemon(name){
+            this.newPokemon = name;
+            this.showPokemon();
         }
     },
 });
--- cliente/pokeApp/index.js
+++ cliente/pokeApp/index.js
@@ -1,10 +1,30 @@
-import { fetchPokemon, fetchPokemonA, axiosPokemon, fetchPokemonInfo, axiosPokemonInfo, fetchPokemonInfoA } from './pokemon.mjs';
+import axios from 'axios';
+
+// Lista de pokemoin con axios
+const axiosPokemon = async (limit) => {
+    try {
+      const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=${limit}/`)
+      return response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
+//pokemon con axios
+const axiosPokemonInfo = async (name) => {
+    try {
+      const response = await axios.get("https://pokeapi.co/api/v2/pokemon/" + name);
+      return response.data;
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 //lista con axios
-console.log(await axiosPokemon(10));
+//console.log(await axiosPokemon(10));
 
 //pokemon con axios
 const ditto = await axiosPokemonInfo('ditto');
-
-console.log(dittoObj);
+console.log(ditto);
 
