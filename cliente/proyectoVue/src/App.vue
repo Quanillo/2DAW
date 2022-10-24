699490a5ad76b3349e261b76c82ebefd4e3ffc2d
Juan Garrigos <j.garrigos91@gmail.com>
2022-10-24 12:38:30 +0200

    vue init

---
+++ cliente/proyectoVue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <MyCounter />
+  {{ msg }}
+  <br>
+  <MyOtherCounter />
+  {{ msg2 }}
+</template>
+
+<script>
+import MyCounter from '@/components/MyCounter.vue'
+import MyOtherCounter from '@/components/MyOtherCounter.vue';
+export default {
+  name: 'App',
+  data() {
+    return {
+      msg: 'Hola soy juan',
+      msg2: 'Hola soy otro componente',
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ cliente/proyectoVue/src/components/MyOtherCounter.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  msg2: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>{{msg2}}</h1>
+  </div>
+</template>
+
+<style scoped>
+h1{
+    color: red;
+}
+</style>
