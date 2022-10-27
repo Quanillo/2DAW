3e42d9a18a9258f7663d6adc40c31330d0ecc6b9
Juan Garrigos <j.garrigos91@gmail.com>
2022-10-27 12:36:43 +0200

    27-10

---
+++ cliente/saludo/src/App.vue
@@ -0,0 +1,34 @@
+
+<script>
+import Lista from './components/Lista.vue';
+export default {
+    name: 'App',
+    components:{Lista},
+    data(){
+      return{
+        personas:[
+          {nombre: 'Juan', apellido: 'Garrigos'},
+          {nombre: 'Pablo', apellido: 'Lantigua'},
+          {nombre: 'Victor', apellido: 'Gil'},
+        ],
+        filtro: '',
+      }
+    },
+    computed:{
+        filterPersonas(){
+            if(!this.filtro || this.filtro.length < 3)
+            return this.personas;
+            else
+            return this.personas.filter(x=> x.nombre.includes(this.filtro) || x.apellido.includes(this.filtro));
+        }
+    },
+  }
+</script>
+
+<template>  
+   <input type="text" v-model.trim="filtro"/> 
+    <Lista :tabla="filterPersonas" />
+</template>
+
+<style scoped>
+</style>
---
+++ cliente/saludo/src/components/Lista.vue
@@ -0,0 +1,23 @@
+<template>
+   
+    <ul>
+        <li v-for="(el, index) in tabla" key="index">
+            {{ el.nombre }}
+            {{ el.apellido }}
+        </li>
+    </ul>
+
+</template>
+
+<script>
+export default {
+    name: 'Lista',
+    props:{
+        tabla: {
+            type: Array,
+            required: true
+        },
+    },
+}
+    
+</script>
