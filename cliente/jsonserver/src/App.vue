05bee6ec1c64e69dfb0d23bc64d0549644a0d5d9
Juan Garrigos <j.garrigos91@gmail.com>
2022-12-14 14:04:26 +0100

    avanzando css

--- cliente/jsonserver/src/App.vue
+++ cliente/jsonserver/src/App.vue
@@ -1,11 +1,45 @@
 <template>
-  <h1>jsonserver</h1>
+  <div v-if="!loginOk">
+    <form>
+        <h4>Nombre</h4>
+        <input type="text" v-model="name"/>
+        <h4>Apellidos</h4>
+        <input type="password" v-model="pass"/>
+        <button @click="login">Enviar</button>
+    </form>
+  </div>
+  <div v-else>
+    <p>{{user.value.name}}</p>
+    <p>{{user.value.nivel}}</p>
+  </div>
+
 </template>
 
 <script setup>
+import { ref } from 'vue';
 
 
-console.log()
+const loginOk = ref(false);
+const user = ref({});
+
+const login = async () => {
+  console.log(name.value);
+  console.log(pass.value);
+  try{
+    const response = await axios.get(
+      `http://localhost:3000/user/?name=${name.value}&pass=${pass.value}`
+    );
+    console.log(response.length)
+    response.length > 0 ? loginOk.value = true : loginOk.value = false;
+    user.value = response[0];
+  }
+  catch{
+    console.log('error')
+  }
+  
+
+}
+
 </script>
 
 
---
+++ cliente/jsonserver/src/components/Login.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <form>
+          <h4>Nombre</h4>
+          <input type="text" v-model="name"/>
+          <h4>Apellidos</h4>
+          <input type="password" v-model="pass"/>
+          <button @click="login">Enviar</button>
+      </form>
+    </div>  
+  </template>
+  
+<script setup>
+
+const emits = defineEmits(['login'])
+const name = ref('');
+const pass = ref('');
+  
+const login = () =>{
+    emits('login',e.target.result);
+}
+
+  
+  
+</script>
+  
+  
+  <style scoped>
+  </style>
+  
